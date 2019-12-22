11de7bfc499c410510720842332346a8bdd1069b
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-12-22 21:59:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,26 @@
+def sort_array_asc(array)
+  array.sort
+end 
 
+def sort_array_desc(array)
+  array.sort do |left, right|
+    right <=> left 
+  end 
+end 
+
+def sort_array_char_count(array)
+  array.sort do |left,right|
+    left.length<=>right.length 
+  end 
+end 
+
+def swap_elements(array)
+  array[1], array[2] = array[2],array[1]
+  array
+end 
+
+def reverse_array(array)
+  array.reverse
+end 
+
+def kesha_maker
